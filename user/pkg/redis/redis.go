06f4924eb8f7c6e7908fd592ce6454661fc2fd17
Abdellah Hattabi <kirakira1614@gmail.com>
2023-10-11 14:14:36 +0100

    first commit

---
+++ user/pkg/redis/redis.go
@@ -0,0 +1,27 @@
+package redis
+
+import (
+	"Go-grpc/config"
+	"time"
+
+	"github.com/go-redis/redis"
+)
+
+func NewRedisClient(cfg *config.Config) *redis.Client {
+	redisHost := cfg.Redis.RedisAddr
+
+	if redisHost == "" {
+		redisHost = ":6379"
+	}
+
+	client := redis.NewClient(&redis.Options{
+		Addr:         redisHost,
+		MinIdleConns: cfg.Redis.MinIdleConn,
+		PoolSize:     cfg.Redis.PoolSize,
+		PoolTimeout:  time.Duration(cfg.Redis.PoolTimeout) * time.Second,
+		Password:     cfg.Redis.Password,
+		DB:           cfg.Redis.DB,
+	})
+
+	return client
+}
