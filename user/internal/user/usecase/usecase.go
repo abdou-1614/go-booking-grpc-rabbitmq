9f9f00ef71c1b7ea15d0d42a61b1cc1d4bd645c1
Abdellah Hattabi <kirakira1614@gmail.com>
2023-10-21 23:22:25 +0100

    first commit

---
+++ user/internal/user/usecase/usecase.go
@@ -0,0 +1,52 @@
+package usecase
+
+import (
+	"Go-grpc/internal/model"
+	"Go-grpc/internal/user"
+	"Go-grpc/internal/user/delivery/rabbitmq"
+	"Go-grpc/pkg/logger"
+	"context"
+
+	"github.com/opentracing/opentracing-go"
+	"github.com/pkg/errors"
+)
+
+const (
+	imagesExchange = "images"
+	resizeKey      = "resize_image_key"
+	userUUIDHeader = "user_uuid"
+)
+
+type userUseCase struct {
+	userPGRepo user.PGRepository
+	log        logger.Loggor
+	redRepo    user.RedisRepository
+	amqp       rabbitmq.Publisher
+}
+
+func NewUserUseCase(userPGRepo user.PGRepository, log logger.Loggor, redRepo user.RedisRepository, amqp rabbitmq.Publisher) *userUseCase {
+	return &userUseCase{
+		userPGRepo: userPGRepo,
+		log:        log,
+		redRepo:    redRepo,
+		amqp:       amqp,
+	}
+}
+
+func (u *userUseCase) Regiser(ctx context.Context, user *model.User) (*model.UserResponse, error) {
+	span, ctx := opentracing.StartSpanFromContext(ctx, "userUseCase.Register")
+
+	defer span.Finish()
+
+	if err := user.PrepareToCreate(); err != nil {
+		return nil, errors.Wrap(err, "user.PrepareToCreate")
+	}
+
+	created, err := u.userPGRepo.Create(ctx, user)
+
+	if err != nil {
+		return nil, errors.Wrap(err, "userPGRepo.Create")
+	}
+
+	return created, nil
+}
