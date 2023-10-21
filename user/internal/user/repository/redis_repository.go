72455ce4e3eb9e2f1f055b58b46733473c1b70f6
Abdellah Hattabi <kirakira1614@gmail.com>
2023-10-21 23:22:15 +0100

    first commit

---
+++ user/internal/user/repository/redis_repository.go
@@ -0,0 +1,50 @@
+package repository
+
+import (
+	"Go-grpc/internal/model"
+	"context"
+	"encoding/json"
+	"fmt"
+	"time"
+
+	"github.com/opentracing/opentracing-go"
+	"github.com/pkg/errors"
+	"github.com/redis/go-redis/v9"
+	uuid "github.com/satori/go.uuid"
+)
+
+type userRedisRepository struct {
+	redis      *redis.Client
+	prefix     string
+	expiration time.Duration
+}
+
+func NewUserRedisRepository(redis *redis.Client, prefix string, expiration time.Duration) *userRedisRepository {
+	return &userRedisRepository{
+		redis:      redis,
+		prefix:     prefix,
+		expiration: expiration,
+	}
+}
+
+func (u *userRedisRepository) SaveUser(ctx context.Context, user *model.UserResponse) error {
+	span, ctx := opentracing.StartSpanFromContext(ctx, "userRedisRepository")
+
+	defer span.Finish()
+
+	userBytes, err := json.Marshal(user)
+
+	if err != nil {
+		return errors.Wrap(err, "userRedisRepository.SaveUser.json.Marshal")
+	}
+
+	if err := u.redis.SetEx(ctx, u.createKey(user.ID), string(userBytes), u.expiration).Err(); err != nil {
+		return errors.Wrap(err, "userRedisRepository.SaveUser.json.Marshal")
+	}
+
+	return nil
+}
+
+func (u *userRedisRepository) createKey(userID uuid.UUID) string {
+	return fmt.Sprintf("%s: %s", u.prefix, userID)
+}
