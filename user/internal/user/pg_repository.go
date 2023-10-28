5342aac13384296195336cc5c1dace7f49e70347
Abdellah Hattabi <kirakira1614@gmail.com>
2023-10-28 21:34:09 +0100

    first commit

--- user/internal/user/pg_repository.go
+++ user/internal/user/pg_repository.go
@@ -3,14 +3,8 @@ package user
 import (
 	"Go-grpc/internal/model"
 	"context"
-
-	uuid "github.com/satori/go.uuid"
 )
 
 type PGRepository interface {
 	Create(ctx context.Context, user *model.User) (*model.UserResponse, error)
-	GetByID(ctx context.Context, userID uuid.UUID) (*model.UserResponse, error)
-	Update(ctx context.Context, user *model.UserUpdate) (*model.UserResponse, error)
-	UpdateAvatar(ctx context.Context, data *model.UpdateAvatarMsg) error
-	GetUsersByIDs(ctx context.Context, userIDs []string) ([]*model.UserResponse, error)
 }
