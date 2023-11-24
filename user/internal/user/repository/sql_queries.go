27b233c94655a2cccc205cac48cea89ae4034687
Abdellah Hattabi <kirakira1614@gmail.com>
2023-11-24 20:30:38 +0100

    first commit

--- user/internal/user/repository/sql_queries.go
+++ user/internal/user/repository/sql_queries.go
@@ -1,7 +1,7 @@
 package repository
 
 const (
-	createUserQuery = `INSERT INTO users (first_name, last_name, email, password, avatar, role) 
-	VALUES ($1,$2,$3,$4,$5,$6) 
-	RETURNING user_id, first_name, last_name, email, avatar, role, updated_at, created_at`
+	createUserQuery = `INSERT INTO users (first_name, last_name, email, password)
+	VALUES ($1, $2, $3, $4)
+	RETURNING id, first_name, last_name, email, updated_at, created_at`
 )
