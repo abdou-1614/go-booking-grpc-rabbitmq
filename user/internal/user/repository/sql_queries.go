df1dd89cd3832de0fe22ec1065ff52c4aae4a322
Abdellah Hattabi <kirakira1614@gmail.com>
2023-11-26 20:38:49 +0100

    first commit

--- user/internal/user/repository/sql_queries.go
+++ user/internal/user/repository/sql_queries.go
@@ -4,4 +4,6 @@ const (
 	createUserQuery = `INSERT INTO users (first_name, last_name, email, password)
 	VALUES ($1, $2, $3, $4)
 	RETURNING id, first_name, last_name, email, updated_at, created_at`
+
+	getUserByIDQuery = `SELECT id, first_name, last_name, email, role, created_at, updated_at FROM users WHERE id = $1`
 )
