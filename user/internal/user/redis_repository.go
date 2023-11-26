8ba174a150cdaacf87b05351bc7babc0192cfbdd
Abdellah Hattabi <kirakira1614@gmail.com>
2023-11-26 20:38:21 +0100

    first commit

--- user/internal/user/redis_repository.go
+++ user/internal/user/redis_repository.go
@@ -3,8 +3,11 @@ package user
 import (
 	"Go-grpc/internal/model"
 	"context"
+
+	uuid "github.com/satori/go.uuid"
 )
 
 type RedisRepository interface {
 	SaveUser(ctx context.Context, user *model.UserResponse) error
+	GetUserID(ctx context.Context, id uuid.UUID) (*model.UserResponse, error)
 }
