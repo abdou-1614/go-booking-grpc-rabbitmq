98f8eec10d557f5ea9054e89ee73db8943dec608
Abdellah Hattabi <kirakira1614@gmail.com>
2023-11-26 20:38:27 +0100

    first commit

--- user/internal/user/usecase.go
+++ user/internal/user/usecase.go
@@ -3,8 +3,11 @@ package user
 import (
 	"Go-grpc/internal/model"
 	"context"
+
+	uuid "github.com/satori/go.uuid"
 )
 
 type UseCase interface {
 	Register(ctx context.Context, user *model.User) (*model.UserResponse, error)
+	GetByID(ctx context.Context, id uuid.UUID) (*model.UserResponse, error)
 }
