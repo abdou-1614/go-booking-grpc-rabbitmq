b229dd73a8e9a8231afa372bfa541842c1b8a781
Abdellah Hattabi <kirakira1614@gmail.com>
2023-12-01 22:28:55 +0100

    first commit

--- user/internal/user/usecase.go
+++ user/internal/user/usecase.go
@@ -5,9 +5,12 @@ import (
 	"context"
 
 	uuid "github.com/satori/go.uuid"
+	"github.com/streadway/amqp"
 )
 
 type UseCase interface {
 	Register(ctx context.Context, user *model.User) (*model.UserResponse, error)
 	GetByID(ctx context.Context, id uuid.UUID) (*model.UserResponse, error)
+	UpdateUploadedAvatar(ctx context.Context, delevery amqp.Delivery) error
+	UpdateAvatar(ctx context.Context, data *model.UpdateAvatarMsg) error
 }
