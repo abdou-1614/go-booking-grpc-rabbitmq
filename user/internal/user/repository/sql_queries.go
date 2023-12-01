c2a5a083d5e8135d4ba3863224b75aeeeb3f6a82
Abdellah Hattabi <kirakira1614@gmail.com>
2023-12-01 22:29:22 +0100

    first commit

--- user/internal/user/repository/sql_queries.go
+++ user/internal/user/repository/sql_queries.go
@@ -1,9 +1,9 @@
 package repository
 
 const (
-	createUserQuery = `INSERT INTO users (first_name, last_name, email, password)
-	VALUES ($1, $2, $3, $4)
-	RETURNING id, first_name, last_name, email, updated_at, created_at`
+	createUserQuery = `INSERT INTO users (first_name, last_name, email, password, role)
+	VALUES ($1, $2, $3, $4, $5)
+	RETURNING id, first_name, last_name, email, role ,updated_at, created_at`
 
 	getUserByIDQuery = `SELECT id, first_name, last_name, email, role, created_at, updated_at FROM users WHERE id = $1`
 )
