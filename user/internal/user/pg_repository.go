19e5426de36a8d21fcc0148909e4d8336ac05ac9
Abdellah Hattabi <kirakira1614@gmail.com>
2023-12-07 21:20:34 +0100

    first commit

--- user/internal/user/pg_repository.go
+++ user/internal/user/pg_repository.go
@@ -11,4 +11,5 @@ type PGRepository interface {
 	Create(ctx context.Context, user *model.User) (*model.UserResponse, error)
 	GetByID(ctx context.Context, id uuid.UUID) (*model.UserResponse, error)
 	UpdateAvatar(ctx context.Context, data model.UploadedImageMsg) (*model.UserResponse, error)
+	GetByEmail(ctx context.Context, email string) (*model.UserResponse, error)
 }
