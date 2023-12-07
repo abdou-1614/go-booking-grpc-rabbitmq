582acbb8fb610a7c2f28464e8741b87c5f27858c
Abdellah Hattabi <kirakira1614@gmail.com>
2023-12-07 21:21:04 +0100

    first commit

--- user/internal/user/repository/sql_queries.go
+++ user/internal/user/repository/sql_queries.go
@@ -5,6 +5,8 @@ const (
 	VALUES ($1, $2, $3, $4, $5)
 	RETURNING id, first_name, last_name, email, role ,updated_at, created_at`
 
+	getUserByEmail = `SELECT id, first_name, last_name, email, role, updated_at, created_at FROM users WHERE email = $1`
+
 	getUserByIDQuery  = `SELECT id, first_name, last_name, email, role, created_at, updated_at FROM users WHERE id = $1`
 	updateAvatarQuery = `UPDATE users SET avatar = $1 WHERE id = $2 `
 )
