b01df1a0e0aca86607570f850c0b0af1f920ec88
Abdellah Hattabi <kirakira1614@gmail.com>
2023-12-13 21:52:12 +0100

    first commit

---
+++ auth/internal/model/session.go
@@ -0,0 +1,39 @@
+package model
+
+import (
+	"time"
+
+	uuid "github.com/satori/go.uuid"
+)
+
+type Session struct {
+	ID           uuid.UUID `json:"id"`
+	Email        string    `json:"email" validate:"required,email"`
+	RefreshToken string    `json:"refresh_token"`
+	UserAgent    string    `json:"user_agent"`
+	ClientIP     string    `json:"client_ip"`
+	IsBlocked    bool      `json:"is_blocked"`
+	ExpiresAt    time.Time `json:"expires_at"`
+	CreatedAt    time.Time `json:"created_at"`
+}
+
+type SessionResponse struct {
+	ID           uuid.UUID `json:"id"`
+	Email        string    `json:"email" validate:"required,email"`
+	RefreshToken string    `json:"refresh_token"`
+	UserAgent    string    `json:"user_agent"`
+	ClientIP     string    `json:"client_ip"`
+	IsBlocked    bool      `json:"is_blocked"`
+	ExpiresAt    time.Time `json:"expires_at"`
+	CreatedAt    time.Time `json:"created_at"`
+}
+
+type CreateSessionParams struct {
+	ID           uuid.UUID `json:"id"`
+	Email        string    `json:"email"`
+	RefreshToken string    `json:"refresh_token"`
+	UserAgent    string    `json:"user_agent"`
+	ClientIP     string    `json:"client_ip"`
+	IsBlocked    bool      `json:"is_blocked"`
+	ExpiresAt    time.Time `json:"expires_at"`
+}
