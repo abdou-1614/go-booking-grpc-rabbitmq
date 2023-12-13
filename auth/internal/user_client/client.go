c1a811a24ab451bd43094295f57a367a28a9407c
Abdellah Hattabi <kirakira1614@gmail.com>
2023-12-13 21:52:18 +0100

    first commit

---
+++ auth/internal/user_client/client.go
@@ -0,0 +1,38 @@
+package userclient
+
+import (
+	"Go-grpc/config"
+	"auth/internal/interceptors"
+	"context"
+	"time"
+
+	grpc_retry "github.com/grpc-ecosystem/go-grpc-middleware/retry"
+	traceutils "github.com/opentracing-contrib/go-grpc"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+const (
+	backoffLinear = 100 * time.Millisecond
+)
+
+func NewUserServiceConn(ctx context.Context, cf *config.Config, interceptor *interceptors.InterceptorManager) (*grpc.ClientConn, error) {
+	opts := []grpc_retry.CallOption{
+		grpc_retry.WithBackoff(grpc_retry.BackoffLinear(backoffLinear)),
+		grpc_retry.WithCodes(codes.NotFound, codes.Aborted),
+	}
+
+	userGRPCConn, err := grpc.DialContext(
+		ctx,
+		cf.GRPCServer.SessionGrpcServicePort,
+		grpc.WithUnaryInterceptor(traceutils.OpenTracingClientInterceptor(interceptor.GetTracer())),
+		grpc.WithUnaryInterceptor(interceptor.GetInterceptor()),
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+		grpc.WithUnaryInterceptor(grpc_retry.UnaryClientInterceptor(opts...)),
+	)
+	if err != nil {
+		return nil, err
+	}
+	return userGRPCConn, nil
+}
