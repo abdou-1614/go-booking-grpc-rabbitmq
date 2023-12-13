7b0a3cf052378f75399cd227a1fd866f692584cf
Abdellah Hattabi <kirakira1614@gmail.com>
2023-12-13 21:52:26 +0100

    first commit

---
+++ auth/migrations/000001_add_sessions.up.sql
@@ -0,0 +1,9 @@
+CREATE TABLE "sessions" (
+  "id" uuid PRIMARY KEY,
+  "refresh_token" varchar NOT NULL,
+  "user_agent" varchar NOT NULL,
+  "client_ip" varchar NOT NULL,
+  "is_blocked" boolean NOT NULL DEFAULT false,
+  "expires_at" timestamptz NOT NULL,
+  "created_at" timestamptz NOT NULL DEFAULT (now())
+);
