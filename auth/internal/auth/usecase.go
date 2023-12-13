1fe40e389ee58e83c8dce35978353238542cb9ae
Abdellah Hattabi <kirakira1614@gmail.com>
2023-12-13 21:51:51 +0100

    first commit

--- auth/internal/auth/usecase.go
+++ auth/internal/auth/usecase.go
@@ -1,1 +1,10 @@
 package auth
+
+import (
+	"auth/internal/model"
+	"context"
+)
+
+type SessUseCase interface {
+	Login(ctx context.Context, arg *model.CreateSessionParams) (*model.SessionResponse, error)
+}
