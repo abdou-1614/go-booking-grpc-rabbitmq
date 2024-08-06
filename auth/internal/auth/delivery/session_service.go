3652aa4ece259e5fcf8394adf52022fb6df91e78
Abdellah Hattabi <kirakira1614@gmail.com>
2024-08-06 23:36:45 +0100

    (grpc-kafka): grpc_gateway implementation

--- auth/internal/auth/delivery/session_service.go
+++ auth/internal/auth/delivery/session_service.go
@@ -4,6 +4,10 @@ import (
 	"auth/internal/auth"
 	sessionService "auth/pb/sessions"
 	"auth/pkg/logger"
+	"context"
+
+	"github.com/opentracing/opentracing-go"
+	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
 type SessionGRPCService struct {
@@ -17,4 +21,21 @@ func NewSessionGRPCService(sessUseCase auth.SessUseCase, log logger.Logger) *Ses
 		sessUseCase: sessUseCase,
 		log:         log,
 	}
+}
+
+func (s *SessionGRPCService) CreateSession(ctx context.Context, req *sessionService.LoginUserRequest) (*sessionService.LoginUserResponse, error) {
+	span, ctx := opentracing.StartSpanFromContext(ctx, "userServiceGrpc.Register")
+	defer span.Finish()
+	res, err := s.sessUseCase.CreateSession(ctx, req.Email, req.Password)
+	if err != nil {
+		return nil, err
+	}
+
+	return &sessionService.LoginUserResponse{
+		SessionId:             res.SessionID,
+		AccessToken:           res.AccessToken,
+		RefreshToken:          res.RefreshToken,
+		AccessTokenExpiresAt:  timestamppb.New(res.AccessTokenExpiresAt),
+		RefreshTokenExpiresAt: timestamppb.New(res.RefreshTokenExpiresAt),
+	}, nil
 }
